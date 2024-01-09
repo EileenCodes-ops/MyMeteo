cf69adc8a9c67715033f9b4db261c1d36022dd2a
EileenCodes-ops <145449239+EileenCodes-ops@users.noreply.github.com>
2024-01-09 13:27:11 +0000

    Fixed coding mistakes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-from-input");
+  let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.ariaValueMax;
 }
 
-let searchFromElement = document.querySelector("#search-from");
-searchFromElement.addEventListener("submit", handleSearch);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearch);
